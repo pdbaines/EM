8d7a0381a28248b87e9780acc507e8ec33704302
Paul Baines <pdbaines@ucdavis.edu>
2013-01-29 18:25:57 -0800

    Added EM_example.R

---
+++ test/EM_example.R
@@ -0,0 +1,131 @@
+
+# Start with clean slate:
+rm(list=ls())
+
+library(EM)
+
+outdir <- "~/Dropbox/"
+
+"EM.example.update" <- function(theta,y.obs,fixed,verbose)
+{
+  y1 <- y.obs$y1
+  y4 <- y.obs$y4
+  n <- y.obs$n
+  sigma.mc <- fixed$sigma.mc
+  
+  "mstep" <- function(y12, y11, y4, n)
+  {
+    return((y12+y4)/(n-y11))
+  }
+  "estep" <- function(psi_current, y1)
+  {
+    y11 <- (y1/2)/((1/2) + (psi_current/4))
+    y12 <- y1 - y11
+    return(list("y11"=y11,"y12"=y12))
+  }
+
+  E.vals <- estep(psi_current=theta, y1=y1)
+  psi <- rnorm(n=1,mean=mstep(y12=E.vals$y12, y11=E.vals$y11, y4, n),sd=sigma.mc)
+  
+  return(psi)
+}
+
+
+
+y.obs <- list("y1"=125,"y2"=18,"y3"=20,"y4"=34,"n"=125+18+20+34)
+fixed <- list("sigma.mc"=0)
+theta.0 <- 0.5
+max.iter <- 1000
+verbose <- FALSE
+
+t1 <- EM(y.obs=y.obs, fixed=fixed, theta.0=theta.0, update=EM.example.update, max.iter=max.iter, verbose=verbose)
+t1
+
+set.seed(512040017)
+
+max.iter <- 5000000 # 5 million
+print.every <- 100000
+zero.tol <- 0.0
+sigma.mc <- 0.01
+fixed <- list("sigma.mc"=sigma.mc)
+# Never allow convergence:
+t2.time <- system.time({
+  t2 <- EM(y.obs=y.obs, fixed=fixed, theta.0=theta.0, update=EM.example.update, max.iter=max.iter, print.every=print.every, tol=zero.tol, verbose=verbose)
+})
+
+print(t2.time)
+
+t2$theta
+mean(t2$paths$theta)
+t1$theta
+
+rs <- cumsum(t2$paths$theta)/seq(along=t2$paths$theta) 
+if (max.iter>1000000){
+  pdf(paste(outdir,"mcem_long_run.pdf",sep=""))
+} else {
+  pdf(paste(outdir,"mcem_short_run.pdf",sep=""))
+}
+plot(rs,type="l",ylim=t1$theta+c(-0.0001,0.0001),ylab="theta",main="Monte Carlo EM: Long-run Average",
+     xlab="iteration",sub=paste("sigma.mc=",sigma.mc,sep=""))
+abline(h=mean(t2$paths$theta),col="red")
+abline(h=mean(t1$theta),col="blue")
+legend("topright",legend=c("MLE","Long-run Average"),col=c("blue","red"),lty=1)
+dev.off()
+
+pdf(paste(outdir,"MCEM_example_mapping.pdf",sep=""))
+plot(x=t2$paths$theta[1:(t2$iterations-1)],
+     y=t2$paths$theta[2:t2$iterations],cex.pt=0.6,
+     xlab="theta^{(t)}",ylab="theta^{(t+1)}",
+     main="MCEM Stochastic Update Mapping")
+dev.off()
+
+save.image(paste(outdir,"large_sim.RData",sep=""))
+
+library(coda)
+
+pdf(paste(outdir,"t2_path.pdf",sep=""))
+plot(mcmc(t2$paths$theta))
+dev.off()
+
+ss.MC.reps <- 10
+mu.start <- 0
+sigma.start <- 1e5
+iter.to.wait <- 5
+cr <- 0.95
+n.monitor.samples <- 10000
+max.iter <- 1000000 # 1 million
+verbose <- FALSE # TRUE
+print.every <- 10000
+
+t3 <- MCEM(y.obs=y.obs, fixed=fixed, theta.0=theta.0, update=EM.example.update, 
+           max.iter=max.iter, monitor=TRUE, ss.MC.reps=ss.MC.reps, 
+           mu.start=mu.start, sigma.start=sigma.start,
+           iter.to.wait=iter.to.wait, cr=cr,n.monitor.samples=n.monitor.samples,
+           print.every=print.every, tol=zero.tol, verbose=verbose)
+
+t3$paths$theta
+t3$paths$theta.tilde
+t3$paths$err
+t3$paths$errq
+
+c("true.MLE"=t1$theta, # true MLE
+  "Long.run.mean"=mean(t2$paths$theta), # long-run mean
+   "Our.estimate"=t3$paths$theta.tilde[length(t3$paths$theta.tilde)]) # our estimate
+
+matplot(cbind(t3$paths$theta,t3$paths$theta.tilde),type="l")
+
+pdf(paste(outdir,"mcem_comparison.pdf",sep=""))
+rs2 <- cbind(rs[1:t3$iterations],t3$paths$theta.tilde)
+matplot(rs2,type="l",ylab="theta",main="Monte Carlo EM: Comparison",
+        ylim=t1$theta+c(-0.0003,0.0003),
+        xlab="iteration",sub=paste("sigma.mc=",sigma.mc,sep=""),
+        col=c("red","orange"),lty=1)
+abline(h=mean(t2$paths$theta[1:t3$iterations]),col="red")
+abline(h=mean(t1$theta),col="blue")
+abline(h=t3$paths$theta.tilde[length(t3$paths$theta.tilde)],col="orange")
+legend("bottomright",legend=c("MLE","Long-run Average","tilde-estimate"),
+       col=c("blue","red","orange"),lty=1)
+dev.off()
+
+plot(log(t3$paths$err),type="l",main="MCEM Estimate Error",ylab="log(err)",xlab="Iteration")
+plot(log(t3$paths$errq),type="l",main="MCEM Quantile Range",ylab="log(QR)",xlab="Iteration")
